65ee5e8f7f4e40a11997485dab97c91fab877c97
wxq <mystart_dream@163.com>
2019-02-19 21:04:56 +0800

    测试IDEA集成Git

--- test_github/src/TestGit.java
+++ test_github/src/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
     public static void run(){
         System.out.println("测试GitHub集成环境搭建");
         System.out.println("怎么不能连上呢");
+        System.out.println("终于连接上了");
     }
 }
